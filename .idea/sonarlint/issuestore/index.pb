f2164287d4bdf15b3252081f0d3ae2c830aceb81
Leo <drd9811@naver.com>
2022-12-04 17:49:09 +0900

    값 타입 매핑

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+d
+4src/main/java/jpabook/jpashop/domain/BaseEntity.java,e\7\e73d1ae5747079484e4207fbea913c358e3e6b06
---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Address {
+    @Column(length = 10)
+    private String city;
+    @Column(length = 20)
+    private String street;
+    @Column(length = 5)
+    private String zipcode;
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    private String fullAddress() {
+        return getCity() + " " + getStreet() + " " + getZipcode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+}
